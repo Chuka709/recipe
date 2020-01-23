d7bfed1da88bfcc26cde0dbc2dbc7cb0129edff5
Chuka <b.chuka@gmail.com>
2020-01-23 17:36:08 +0800

    search icon gargana

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 require("@babel/polyfill");
 import doSearch from "./model/Search";
 import Search from "./model/Search";
-
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 /**
  * Web app төлөв
  * - Хайлтын query, үр дүн
@@ -12,20 +13,27 @@ import Search from "./model/Search";
 const state = {};
 const controlSearch = async () => {
   //1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
-  const query = "pizza";
+  const query = searchView.getInput();
 
   if (query) {
     //2. Шинээр хайлтын объектыг үүсгэж өгнө.
     state.search = new Search(query);
     //3. Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
-
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
     //4. Хайлтыг гүйцэтгэнэ.
     await state.search.doSearch();
     //5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
-    console.log(state.search.result);
+    clearLoader();
+    if (state.search.result === undefined) {
+      alert("Хайлтаар илэрцгүй...    ");
+    } else {
+      searchView.renderRecipes(state.search.result);
+    }
   }
 };
-document.querySelector(".search").addEventListener("submit", e => {
+elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
